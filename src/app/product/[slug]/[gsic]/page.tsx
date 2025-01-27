3c2598b2f5ea31a451b35d9b29a870774e013fd6
Anderson Vieira <asvieira.br@gmail.com>
2025-01-27 09:27:37 -0300

    feat: enhance product detail page layout and add AppHeader component

--- src/app/product/[slug]/[gsic]/page.tsx
+++ src/app/product/[slug]/[gsic]/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import AppHeader from "@/module/shared/component/AppHeader";
 import { useParams } from "next/navigation";
 import { useEffect } from "react";
 import { useProductStore } from "../../store/product.store";
@@ -20,17 +21,31 @@ export default function ProductDetail() {
   }
 
   return (
-    <div className="container mx-auto px-4 py-8">
-      <h1 className="text-3xl font-bold">{product.name}</h1>
-      <p className="text-lg mt-4">{product.description}</p>
-      <p className="text-2xl font-semibold text-green-600 mt-4">
-        R$ {product.price.toFixed(2)}
-      </p>
-      <img
-        src={product.imageUrl}
-        alt={product.name}
-        className="mt-8 rounded-lg"
-      />
-    </div>
+    <>
+      <AppHeader />
+      <div className="mx-auto px-60 py-12 min-h-screen">
+        <div className="grid grid-cols-2 gap-8 max-w-[800px] mx-auto">
+          <div>
+            <h1 className="text-2xl font-bold">{product.name}</h1>
+            <img
+              width={400}
+              src={product.imageUrl}
+              alt={product.name}
+              className="mt-8 rounded-lg"
+            />
+            <p className="text-lg mt-4">{product.description}</p>
+          </div>
+          <div className="flex flex-col gap-4 justify-between">
+            <div className="flex-grow"></div>
+            <p className="text-2xl font-semibold text-zinc-600 mt-4">
+              R$ {product.price.toFixed(2)}
+            </p>
+            <button className="bg-purple-600 hover:bg-purple-800 text-white px-6 py-4 rounded text-xs font-bold uppercase">
+              Adicionar à sacola
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
