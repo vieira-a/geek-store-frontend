f0a2aebeb40df74439c372d9d0a651dbd81d0937
Anderson Vieira <asvieira.br@gmail.com>
2025-01-29 19:32:09 -0300

    feat: update AppHeader component to include navigation links and improve layout

--- src/module/shared/component/AppHeader.tsx
+++ src/module/shared/component/AppHeader.tsx
@@ -6,14 +6,16 @@ export default function AppHeader() {
   return (
     <>
       <div className="px-60 py-12 bg-zinc-950 flex justify-between items-center">
-        <Image alt="Geek Store" width={110} height={14} src={Logo} />
+        <Link href="/">
+          <Image alt="Geek Store" width={110} height={14} src={Logo} />
+        </Link>
         <nav className="text-slate-100">
           <ul>
             <li>
-              <Link href="/produtos">Produtos</Link>
+              <Link href="/">Produtos</Link>
             </li>
             <li>
-              <Link href="/cart">Carrrinho</Link>
+              <Link href="/cart">Carrinho</Link>
             </li>
           </ul>
         </nav>
