55ef6f02f3a1fef77c162f3d51d353bb190e474d
Anderson Vieira <asvieira.br@gmail.com>
2025-01-29 19:32:17 -0300

    refactor: remove unused AppHeader import from Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import FeaturedProducts from "@/app/product/component/FeaturedProducts";
-import AppHeader from "@/module/shared/component/AppHeader";
 import AppHero from "@/module/shared/component/AppHero";
 
 export default function Home() {
